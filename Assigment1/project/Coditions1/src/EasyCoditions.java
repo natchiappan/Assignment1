52c29af2796e06b2c971cfe5ffab936910e05b9a
natchiappan <nashjaden@yahoo.com>
2015-03-29 15:38:56 +0100

    just testing

---
+++ Assigment1/project/Coditions1/src/EasyCoditions.java
@@ -0,0 +1,47 @@
+/*
+
+        
+    
+    
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Natchiappan
+ */
+public class EasyCoditions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+               
+    
+   int age = 10;
+   int bmi =20;
+   int weight =40;
+
+      
+        if (age>60)
+                System.out.println("You are a Senior Citizen");
+
+   else{
+            System.out.println("You are NOT Senior Citizen");
+}
+        if(bmi>20)
+            System.out.println("you are Obesity");
+        else{
+            System.out.println("you are NOT Obesity");
+        }
+        if(weight>40)
+            System.out.println("you are pefect");
+        else{
+            System.out.println("You are NOT Perfect");
+        }
+    }
+        
